5e777deaaaeb3ea345e6cb49ef55c2921afe6b6c
Yoel Aliaga <112524404+Yoel-Aliaga@users.noreply.github.com>
2022-11-30 19:01:26 -0500

    nosotros mision vision material ui

--- src/pages/Nosotros.js
+++ src/pages/Nosotros.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Grid, Card, Box } from "@mui/material";
 
 export const Nosotros = () => {
   return (
-
- <div style={{marginTop:"10px"}}>
-
-  Nosotros
-      <p>454  </p>
-      <p>454  </p>
-      <p>454  </p>
-      <p>454  </p>
-      <p>454  </p>
-      <p>454  </p>
-      <p>454  </p>
-      <p>454  </p>
-      <p>454  </p>
+    <div>
+      <Grid container justifyContent="center">
+        <Grid item xs={8} sm={8} md={8} marginTop="50px">          
+          <Card variant="outlined" style={{ padding: "20px", boxShadow:"0px 4px 20px 1px rgba(0,0,0, 0.2)"}}>
+            <h2 style={{ textAlign: "center", marginBottom:"20px" }}>Misión</h2>
+            <div>
+              Estamos centrados en ofrecer los mejores servicios de recreación,
+              esparcimiento, deportes y diversión como una nueva alternativa
+              para la utilización del tiempo libre de grupos sociales,
+              empresariales y familiares, con unas instalaciones bien equipadas,
+              diseñadas estéticamente y con un equipo humano especializado en
+              nuestros servicios
+            </div>
+          </Card>
+        </Grid>
+        
+        <Grid item xs={8} sm={8} md={8} marginTop="100px">
+          <Card variant="outlined" style={{ padding: "20px", boxShadow:"0px 4px 20px 1px rgba(0,0,0, 0.2)" }}>
+            <h2 style={{ textAlign: "center", marginBottom:"20px"}}>Visión</h2>
+            <div>
+              Ser un centro turístico y recreativo caracterizado por poseer una
+              gran variedad de alternativas de entretenimiento, recreación
+              campestre, ambientación natural y hospedaje de alta calidad para
+              el beneficio de nuestros clientes. Con un personal altamente
+              capacitado en estrategias de atención personalizada.
+            </div>
+          </Card>
+        </Grid>
+      </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default Nosotros
+export default Nosotros;
